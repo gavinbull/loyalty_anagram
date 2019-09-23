ac99e5192e24d32a978a8ca5ac423a6f47e14011
gavinbull <47794215+gavinbull@users.noreply.github.com>
2019-09-23 01:17:33 -0400

    Update anagram.py
    Update gather_anagrams function to remove itertools

--- anagram.py
+++ anagram.py
@@ -1,12 +1,16 @@
 import json
-import itertools
 
 def gather_anagrams(input_word):
-    anagrams_list = [ "".join(all_permutations) for all_permutations in itertools.permutations(input_word) if len(all_permutations) == len(input_word) ]
-    return anagrams
+    if len(input_word) <=1:
+        return input_word
+    else:
+        anagrams = []
+        for perm in gather_anagrams(input_word[1:]):
+            for i in range(len(input_word)):
+                anagrams.append(perm[:i] + input_word[0:1] + perm[i:])
+        return anagrams
 
 def lambda_handler(event, context):
-    return event
     # define input word and set output anagrams
     input_word = ""
     anagrams = ["invalid request"]
