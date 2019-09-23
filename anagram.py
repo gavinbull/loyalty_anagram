5f5270a3bcb539a96c8569107ea7e07050d59622
gavinbull <47794215+gavinbull@users.noreply.github.com>
2019-09-23 01:27:13 -0400

    Update anagram.py
    Use itertools in gather_anagrams to make code cleaner

--- anagram.py
+++ anagram.py
@@ -1,24 +1,20 @@
-import json
+import json, itertools
 
 def gather_anagrams(input_word):
-    if len(input_word) <=1:
-        return input_word
-    else:
-        anagrams = []
-        for perm in gather_anagrams(input_word[1:]):
-            for i in range(len(input_word)):
-                anagrams.append(perm[:i] + input_word[0:1] + perm[i:])
-        return anagrams
+    anagrams = sorted(set(["".join(permutation) for permutation in itertools.permutations(input_word)]))
+    return anagrams
 
 def lambda_handler(event, context):
     # define input word and set output anagrams
     input_word = ""
     anagrams = ["invalid request"]
+    statuscode = 400
     try:
         input_word = event['queryStringParameters']['word']
         if input_word:
             # if word is present, find anagrams
             anagrams = gather_anagrams(input_word)
+            statuscode = 200
     except:
         pass
 
@@ -26,9 +22,8 @@ def lambda_handler(event, context):
     message = {
         'anagrams': '%s' % str(anagrams)
     }
-
     return {
-        'statusCode': 200,
+        'statusCode': statuscode,
         'headers': {'Content-Type': 'application/json'},
         'body': json.dumps(message)
     }
